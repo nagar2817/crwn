0966f52463c963edc7d565dee7e2f00bcbdf0d30
@nagar2817 <nagar.2@iitj.ac.in>
2020-09-04 15:49:33 +0530

    add class component instead functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import HomePage from './homepage.component';
 
-function App() {
-  return (
-    <div >
-      <HomePage />
-    </div>
-  );
+// function App() {
+//   return (
+//    <div >
+// <HomePage />
+// </div>
+//   );
+// }
+
+class App extends Component{
+ 
+  render(){
+    return (
+      <div >
+             <HomePage />
+         </div>
+    );
+  }
+  
 }
 
 export default App;
